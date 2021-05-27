264bef8fdf03e7fcb81d862136c9c21560daeb7d
Erick desktop <erickrhein@gmail.com>
2021-05-28 00:14:05 +0200

    mocking a json object

--- src/index.js
+++ src/index.js
@@ -1,11 +1,51 @@
 import { GraphQLServer } from "graphql-yoga";
 
+//Demo user data
+const users = [
+  {
+    id: "1",
+    name: "Don Rick",
+    email: "rick@rico.com",
+    age: 29,
+  },
+  {
+    id: "2",
+    name: "Patricio",
+    email: "pat@tricio.com",
+    age: 18,
+  },
+  {
+    id: "Sandy",
+    name: "Sandy",
+    email: "sandy@sand.com",
+  },
+];
+
+const posts = [
+  {
+    id: "1",
+    title: "Don Quijote",
+    body: "En un lugar de la Mancha de cuyo nombre no quiero acordarme",
+    published: true,
+  },
+  {
+    id: "2",
+    title: "El cid",
+    body: "Erase una vez un patito feo que se convirtio en un Ogro verde. Luego salvo a la princesa y todos vivieron felices por siempre.",
+    published: false,
+  },
+  {
+    id: "3",
+    title: "Mi noches de soledad",
+    body: "Quizas amaste a quien no debiste amar, tomaste una decision fatal, te lastimaron y eso to hizo mal. Y yo lo tuve que pagar. QUIZAS!!",
+    published: true,
+  },
+];
 //Type definitions (schema)
 const typeDefs = `
     type Query {
-        greeting(name: String, position: String): String!
-        add(a: Float!, b: Float!) : Float!
-        grades: [Int!]!
+        users(query: String): [User!]!
+        posts(query: String): [Post!]!
         me: User!
         post: Post!
         }
@@ -27,18 +67,27 @@ const typeDefs = `
 //Resolvers
 const resolvers = {
   Query: {
-    greeting(parent, args, ctx, info) {
-      if (args.name && args.position) {
-        return `Hello, ${args.name}! You are my favourite ${args.position}`;
-      } else {
-        return `MORGEN!`;
+    users(parent, args, ctx, info) {
+      if (!args.query) {
+        return users;
       }
+      return users.filter((user) => {
+        return user.name.toLowerCase().includes(args.query.toLowerCase());
+      });
     },
-    add(parent, args, ctx, info) {
-      return args.a + args.b;
-    },
-    grades(parent, args, ctx, info) {
-      return [99, 80, 93];
+    posts(parent, args, ctx, info) {
+      if (args.query) {
+        return posts.filter((post) => {
+          const query_in_title = post.title
+            .toLowerCase()
+            .includes(args.query.toLowerCase());
+          const query_in_body = post.body
+            .toLowerCase()
+            .includes(args.query.toLowerCase());
+
+          return query_in_title || query_in_body;
+        });
+      } else return posts;
     },
     me() {
       return {
