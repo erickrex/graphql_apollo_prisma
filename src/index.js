9735da82e19c2170b845222366c6977acb7d8be8
Erick desktop <erickrhein@gmail.com>
2021-05-27 16:44:55 +0200

    testing out some hard coded objects

--- src/index.js
+++ src/index.js
@@ -3,19 +3,58 @@ import { GraphQLServer } from "graphql-yoga";
 //Type definitions (schema)
 const typeDefs = `
     type Query {
-        hello: String!
+        greeting(name: String, position: String): String!
+        add(a: Float!, b: Float!) : Float!
+        grades: [Int!]!
+        me: User!
+        post: Post!
+        }
+
+    type User {
+        id: ID!
         name: String!
+        email: String
+        age: Int
+    }
+    type Post {
+        id: ID!
+        title: String!
+        body: String!
+        published: Boolean!
     }
 `;
 
 //Resolvers
 const resolvers = {
   Query: {
-    hello() {
-      return "Example of first query";
+    greeting(parent, args, ctx, info) {
+      if (args.name && args.position) {
+        return `Hello, ${args.name}! You are my favourite ${args.position}`;
+      } else {
+        return `MORGEN!`;
+      }
     },
-    name() {
-      return "Erick Rex";
+    add(parent, args, ctx, info) {
+      return args.a + args.b;
+    },
+    grades(parent, args, ctx, info) {
+      return [99, 80, 93];
+    },
+    me() {
+      return {
+        id: "123456789",
+        name: "Rico",
+        email: "rick@rico.com",
+        age: 29,
+      };
+    },
+    post() {
+      return {
+        id: "984",
+        title: "Homepage",
+        body: "Example of a post",
+        published: false,
+      };
     },
   },
 };
